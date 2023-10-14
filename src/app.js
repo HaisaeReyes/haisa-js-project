e74e70f3fe9a35506bb17cfdc995ad14db34d9a6
Haisa Reyes <130813009+HaisaeReyes@users.noreply.github.com>
2023-10-14 03:04:05 +0000

    correccion grupal 13/10

--- src/app.js
+++ src/app.js
@@ -28,13 +28,5 @@ let generateExcuse = function() {
   let whatIndex = Math.floor(Math.random() * what.length);
   let whenIndex = Math.floor(Math.random() * when.length);
 
-  return (
-    who[whoIndex] +
-    " " +
-    action[actionIndex] +
-    " " +
-    what[whatIndex] +
-    " " +
-    when[whenIndex]
-  );
+  return `${who[whoIndex]} ${action[actionIndex]} ${what[whatIndex]} ${when[whenIndex]}`;
 };
